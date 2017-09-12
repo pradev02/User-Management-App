44b4671f0b33482c48d0836c66cbf7ec3e2df86d
Pravakar <pra.developer02@gmail.com>
2017-09-12 16:00:37 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+
+## Requirements
+
+Have these packages installed and running on your system.
+
+- [Node.js](https://nodejs.org/download/), and npm.
+- [MongoDB](https://www.mongodb.org/downloads)
+- [SASS](http://sass-lang.com/install)
+- [Grunt-cli](http://gruntjs.com/getting-started)
+- [Bower](http://bower.io/#install-bower)
+
+
+## Installation
+```bash
+
+$ npm install
+$ cd client && bower install && cd ..
+```
+
+## Setup
+
+Interactively setup basic website and necessary database configurations.
+```bash
+$ node init.js
+```
+
+
+## Running the app
+
+```bash
+$ grunt
+
+# > grunt
+
+# Running "clean:src" (clean) task
+# ...
+
+# Running "concurrent:dev" (concurrent) task
+# Running "watch" task
+# Running "nodemon:dev" (nodemon) task
+# Waiting...
+# [nodemon] v1.2.1
+# [nodemon] to restart at any time, enter `rs`
+# [nodemon] watching: *.*
+# [nodemon] starting `node app.js`
+```
+
+Now App should be up and running at `http://localhost:3000`.
---
+++ config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+exports.hostname = process.env.hostname || 'localhost';
+exports.port = process.env.PORT || 3000;
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/sample-app'
+};
+exports.companyName = 'Sample';
+exports.projectName = 'User Management App';
+exports.systemEmail = 'r@r.com';
+exports.cryptoKey = 'abcxaflfwdqsa';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
+    address: process.env.SMTP_FROM_ADDRESS || 'r@r.com'
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || '',
+    password: process.env.SMTP_PASSWORD || '',
+    host: process.env.SMTP_HOST || '',
+    ssl: true
+  }
+};
+exports.oauth = {
+  twitter: {
+    // Not yet implemented
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    // Not yet implemented
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    // Not yet implemented
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
